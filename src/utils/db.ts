164a94a607d03feef60c856ad1549ed8af1a1f1d
Switch <switchanmd@gmail.com>
2023-06-01 09:07:15 +0900

    fix pwd

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,11 @@
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
+const pwd = Deno.env.get("PASSWORD");
+
 export const client = new Client({
   user: "postgres",
   database: "postgres",
   hostname: "db.qxriygfililzsjlmkebb.supabase.co",
   port: 5432,
-  password: "sBcuGp2AAfKrqtEj",
+  password: pwd,
 });
